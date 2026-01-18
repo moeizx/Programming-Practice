e7b144832bafe5cbdf7bed082b30cd8247f65464
Moeiz Mughal <moeizcode@gmail.com>
2026-01-18 17:50:50 +0500

    Add files via upload

--- C/factorial.c
+++ C/factorial.c
@@ -2,24 +2,47 @@
 
 #include <stdio.h>
 
-long factorial(int a);
-
-void leap_year(int a);
+long recursion(int a);
+long iteration(int a);
 
 int main()
 {
+    int choice;
+    printf("1- Factorial through iteration\n2- Factorial through recurion");
+    printf("\nChoice: ");
+    scanf("%d",&choice);
+    if(choice!=1 && choice !=2)
+    {
+        printf("Invalid choice\n");
+        return 0;
+    }
+
     int n;
     printf("Number: ");
     scanf("%d", &n);
+    if(n<0)
+    {
+        printf("Invalid number\n");
+        return 0;
+    }
 
-    long x = factorial(n);
+    long x;
 
+    if(choice==1)
+    {
+
+        x= iteration(n);
+        printf("Factorial of %d: %li\n",n,x);
+        return 0;
+    }
+
+    x = recursion(n);
     printf("Factorial of %d: %li\n", n, x);
-
     return 0;
+
 }
 
-long factorial(int a)
+long recursion(int a)
 {
     // base case
     if (a == 0)
@@ -28,5 +51,19 @@ long factorial(int a)
     }
 
     // recursive case
-    return a * factorial(a - 1);
+    return a * recursion(a - 1);
+}
+
+long iteration(int a)
+{
+    long factorial=a;
+    if(a==0 || a==1)
+    {
+        return 1;
+    }
+    for(int i=1;i<a;i++)
+    {
+        factorial*=i;
+    }
+    return factorial;
 }
