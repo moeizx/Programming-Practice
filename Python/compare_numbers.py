f8841a0e42b68b5a2d9e278f9d9b0a64815e5bb8
Moeiz Mughal <moeizcode@gmail.com>
2026-02-24 15:19:15 +0500

    Add files via upload

---
+++ Python/compare_numbers.py
@@ -0,0 +1,12 @@
+#Use comparison operator to find out whether ‘a’ given variable a is greater than 
+#‘b’ or not. Take a = 34 and b = 80 
+
+
+number1 = int(input("First number: "))
+number2 = int(input("Second number: "))
+
+if number1 > number2:
+    print(f"{number1} is greater than {number2}")
+
+else:
+    print(f"{number1} is not greater than {number2}")
