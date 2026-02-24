fa4dc2ca9bcce1ea14367de7855834c55adcaed9
Moeiz Mughal <moeizcode@gmail.com>
2026-02-24 15:18:22 +0500

    Add files via upload

---
+++ Python/average_two_numbers.py
@@ -0,0 +1,11 @@
+#Write a python program to find an average of two numbers entered by the user.
+
+#take input
+number1 = int(input("First number: "))
+number2 = int(input("Second number: "))
+
+#calculate average
+average = (number1 + number2) / 2
+
+#print result
+print("Average:",average)
