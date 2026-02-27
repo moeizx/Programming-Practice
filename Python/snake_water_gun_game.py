4a1a275d6d51e3ddff35d39659babebad4978d37
Moeiz Mughal <moeizcode@gmail.com>
2026-02-27 10:01:44 +0500

    Add files via upload

---
+++ Python/snake_water_gun_game.py
@@ -0,0 +1,34 @@
+'''
+Write a Python program to implement the Snake-Water-Gun game where the computer
+randomly selects an option and the user enters their choice. 
+Display both choices and determine whether the result is a win, loss, or draw.
+'''
+
+import random
+
+computer = random.choice([1,-1,0])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "Snake", -1: "Water", 0:"Gun"}
+
+you = youDict[youstr]
+
+print(f"You choose: {reverseDict[you]}\nComputer choose: {reverseDict[computer]}")
+
+if computer == you:
+    print("Draw")
+else:
+    if computer == -1 and you == 1:
+        print("You Win!")
+    elif computer == -1 and you == 0:
+        print("You Lose!")
+    elif computer == 1 and you == -1:
+        print("You Lose!")
+    elif computer == 1 and you == 0:
+        print("You Win!")
+    elif computer == 0 and you == 1:
+        print("You Lose!")
+    elif computer == 0 and you == -1:
+        print("You Win!")
+    else:
+        print("Something went wrong")
